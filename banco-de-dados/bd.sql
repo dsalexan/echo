522ecbd5672bd8ad3cd8c91eccdff6c61ff95f9f
Tamires Lucena <tamiresb.lucena@gmail.com>
2018-09-02 20:26:32 -0300

    Inserts Grade

--- banco-de-dados/bd.sql
+++ banco-de-dados/bd.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS saldo;
+﻿DROP TABLE IF EXISTS saldo;
 DROP TABLE IF EXISTS cardapio;
 DROP TABLE IF EXISTS aluno_turma;
 DROP TABLE IF EXISTS compromisso;
@@ -92,11 +92,22 @@ CREATE TABLE materia (
 	creditos INTEGER NOT NULL CHECK (creditos > 0)
 );
 
+INSERT INTO materia
+VALUES (DEFAULT, 'Logica de Programação', 4),
+(DEFAULT, 'Algoritmos e Estrutura de Dados', 4),
+(DEFAULT, 'Calculo em Uma Variavel', 6),
+(DEFAULT, 'Fundamentos da Biologia Moderna', 4),
+(DEFAULT, 'Series e Equaçoes Diferenciais', 4);
+
 CREATE TABLE pre_req (
 	id_materia INTEGER REFERENCES materia(id_materia),
 	id_pre_req INTEGER REFERENCES materia(id_materia),
 	PRIMARY KEY(id_materia, id_pre_req)
 );
+
+INSERT INTO pre_req 
+VALUES (2, 1),
+(5, 3);
 
 CREATE TABLE turma (
 	id_turma SERIAL PRIMARY KEY,
@@ -104,11 +115,26 @@ CREATE TABLE turma (
 	id_professor INTEGER REFERENCES professor(id_professor) NOT NULL
 );
 
+INSERT INTO turma
+VALUES (DEFAULT, 1, 1),
+(DEFAULT, 2, 2),
+(DEFAULT, 3, 3),
+(DEFAULT, 4 ,4),
+(DEFAULT, 5, 5);
+
 CREATE TABLE horario (
 	id_horario SERIAL PRIMARY KEY,
 	dia_semana VARCHAR(3) NOT NULL,
 	hora TIME NOT NULL CHECK ((hora >= '08:00') AND (hora <= '21:00'))
 );
+
+INSERT INTO horario 
+VALUES (DEFAULT, 'SEG', '10:00'),
+(DEFAULT, 'TER', '08:00'),
+(DEFAULT, 'QUA', '19:00'),
+(DEFAULT, 'QUI', '21:00'),
+(DEFAULT, 'SEX', '13:30');
+
 
 create table horario_turma (
 	id_turma INTEGER REFERENCES turma(id_turma) NOT NULL,
@@ -116,6 +142,13 @@ create table horario_turma (
 	id_sala INTEGER REFERENCES sala(id_sala),
 	PRIMARY KEY(id_horario, id_sala)
 );
+
+INSERT INTO horario_turma
+VALUES (1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5);
 
 CREATE TABLE compromisso (
 	id_compromisso SERIAL PRIMARY KEY,
@@ -126,12 +159,27 @@ CREATE TABLE compromisso (
 	informacoes TEXT
 );	
 
+INSERT INTO compromisso
+VALUES (DEFAULT, 1, 'Prova de Calculo', 2018-09-29, 1, 'levar calculadora'),
+(DEFAULT, 2, 'Palestra de Organização Financeira', 2018-10-01, 2, 'vale horas'),
+(DEFAULT, 3, 'Entrega Manuscrito', 2018-10-15, 3, 'Encardenado'),
+(DEFAULT, 4, 'Prova de LP', 2018-11-01, 4, 'até vetores'),
+(DEFAULT, 5, 'Reunião Forgers', 2018-09-4, 4, 'Comparecer com Camiseta do grupo');
+
+
 CREATE TABLE aluno_turma (
 	ra_aluno VARCHAR(6) REFERENCES aluno(ra_aluno),
 	id_turma INTEGER REFERENCES turma(id_turma),
 	faltas INTEGER DEFAULT 0 CHECK (faltas >= 0),
 	PRIMARY KEY(ra_aluno, id_turma)
 );
+
+INSERT INTO aluno_turma
+VALUES('111866', 1, DEFAULT),
+('111867', 2, DEFAULT),
+('111868', 3, DEFAULT),
+('111869', 4, DEFAULT),
+('111820', 5, DEFAULT);
 
 -- ou então
 
