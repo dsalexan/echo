12a7fbb7e60a5ae0c4686e8e51a9b95f2281de2f
dsalexan <danilo.salexan@gmail.com>
2018-09-04 02:06:40 -0300

    setting restful api security

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 
-var app = require('./app')
+var express = require('express')
+var app = express()
 var port = process.env.PORT || 3000
+var router = express.Router()
 
 var server = app.listen(port, () => {
     console.log(`server listening at port ${port}`)
 })
 
 var Test = require('./models/Test')
+var authController = require('./auth/AuthController')
 
-app.get('/api/test/get', (req, res, next) => {
+router.use(function(req, res, next) {
+    console.log(req.method, req.url)
+
+    next()
+})
+
+router.get('/api/test/get', (req, res, next) => {
     Test.getTest().then(data => {
         res.status(200)
             .json({
@@ -21,3 +30,6 @@ app.get('/api/test/get', (req, res, next) => {
         return next(err)
     })
 })
+
+app.use('/api/auth', authController)
+app.use('/', router);
