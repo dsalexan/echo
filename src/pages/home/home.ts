c29e4ffbd7c45f1d89b3876171e0922bfea9181e
gucollaco <gustavo.m.collaco@gmail.com>
2018-10-01 23:28:51 -0300

    Conflicts on home solved

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,14 @@ import { Storage } from '@ionic/storage';
 import { LoginPage } from '../login/login';
 import { CadastroPage } from '../cadastro/cadastro';
 
+import { HTTP } from '@ionic-native/http';
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @IonicPage()
 @Component({
@@ -13,7 +21,7 @@ import { CadastroPage } from '../cadastro/cadastro';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, private http: HTTP) {
   }
 
   checkSession() {
@@ -34,4 +42,12 @@ export class HomePage {
   /*clickLogin() {
     this.navCtrl.push(LoginPage);
   }*/
+  
+
+  clickTeste() {
+    this.http.get('http://localhost:3000/api/grades/get/grade/aluno?ra_aluno=112344', {}, {})
+      .then(data =>
+        document.getElementById("teste").textContent = String(data)
+      )
+  }
 }
