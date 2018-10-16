a96ceab2fbf1a02f4e49bcff5fd1168fb7de2b68
Renata <renata.sendreti@gmail.com>
2018-10-15 23:29:17 -0300

    busca e reserva de carona realizadas

--- src/pages/viagem-passageiro/viagem-passageiro.ts
+++ src/pages/viagem-passageiro/viagem-passageiro.ts
@@ -14,7 +14,41 @@ import { HomePage } from '../home/home';
 })
 export class ViagemPassageiroPage {
 
+  viagem: any;
+  reservaPendente = [];
+  reservaConfirmada = [];
+  loc = {};
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public http: Http, public alertCtrl: AlertController) {
+    this.viagem = this.navParams.get("viagem");
+    this.loc = this.navParams.get("loc");
+  }
+
+  rejeitarReserva(){
+    var path = 'http://localhost:3000/api/caronas/delete/reserva?id=' + this.viagem["id_viagem"]
+    this.http.get(path).map(res => res.json()).subscribe(data => {
+
+      if(data.success) {
+        let alert = this.alertCtrl.create({
+          title: 'Ok!',
+          subTitle: 'Reserva excluída',
+          buttons: ['Dismiss']
+        });
+        alert.present();
+        this.ionViewDidLoad();
+
+        //enviar mensagem de rejeição para o passageiro
+        
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Ops!',
+          subTitle: 'Tente novamente',
+          buttons: ['Dismiss']
+        });
+        alert.present();
+      }
+    })
+
   }
 
   checkSession() {
