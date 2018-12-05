ab73e5c5e712fdcbc4b0dc97c3594f10fde7337f
gucollaco <gustavo.m.collaco@gmail.com>
2018-12-05 13:04:10 -0200

    OK

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -29,31 +29,31 @@ export class LoginPage {
 
   ionViewWillEnter() {
     console.log('ionViewWillEnter LoginPage');
-    var he = new Headers()
-    he.append('Content-Type', 'application/json')
-    let sr = '104.248.9.4'
-    // let sr = 'localhost'
-    // this.http.post('https://webhook.site/47b7ba5c-0fd0-487c-a161-f83921024e02', {'bla': '1'}, {headers: he}).map(res => res.json()).subscribe(result => {
-    // this.http.post(ENV.HOSTNAME + '/teste', {'login': user, 'senha': encryptSenha}, {headers: new HttpHeaders()}).subscribe(result => {
+    // var he = new Headers()
+    // he.append('Content-Type', 'application/json')
+    // let sr = '104.248.9.4'
+    // // let sr = 'localhost'
+    // // this.http.post('https://webhook.site/47b7ba5c-0fd0-487c-a161-f83921024e02', {'bla': '1'}, {headers: he}).map(res => res.json()).subscribe(result => {
+    // // this.http.post(ENV.HOSTNAME + '/teste', {'login': user, 'senha': encryptSenha}, {headers: new HttpHeaders()}).subscribe(result => {
     
-    // this.http.put(ENV.HOSTNAME + '/teste', {'bla': '1'}, {headers: new HttpHeaders().set('Content-Type', 'application/json')}).subscribe(result => {
-    this.http.get(ENV.HOSTNAME + '/teste', {responseType: 'text'}).subscribe(result => {
-      console.log(result)
-      let alert = this.alertCtrl.create({
-        title: 'Nice!',
-        subTitle: 'Servidor Remoto <IG99> OK',
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    }, err => {
-      console.log(err)
-      let alert = this.alertCtrl.create({
-        title: err.name,
-        subTitle: err.message,
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    })
+    // // this.http.put(ENV.HOSTNAME + '/teste', {'bla': '1'}, {headers: new HttpHeaders().set('Content-Type', 'application/json')}).subscribe(result => {
+    // this.http.get(ENV.HOSTNAME + '/teste', {responseType: 'text'}).subscribe(result => {
+    //   console.log(result)
+    //   let alert = this.alertCtrl.create({
+    //     title: 'Nice!',
+    //     subTitle: 'Servidor Remoto <IG99> OK',
+    //     buttons: ['Dismiss']
+    //   });
+    //   alert.present();
+    // }, err => {
+    //   console.log(err)
+    //   let alert = this.alertCtrl.create({
+    //     title: err.name,
+    //     subTitle: err.message,
+    //     buttons: ['Dismiss']
+    //   });
+    //   alert.present();
+    // })
 
     document.getElementById("tabs").style.display = "none"
     document.getElementById("botao_menu").style.display = "none"
